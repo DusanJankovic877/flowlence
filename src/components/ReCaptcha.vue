3af805e09c89b63132c85ba9bd70f5cfe1156536
Dusan <dusanjankovic877@gmail.com>
2021-11-12 10:53:45 +0100

    At ReCaptcha.vue in validate method i have removed everything except emmit event where i'm emmiting captcha token to parent. At recaptchaValidate.js i think i have removed console.log

---
+++ src/components/ReCaptcha.vue
@@ -0,0 +1,19 @@
+<template>
+    <vue-recaptcha :sitekey="siteKey" :loadRecaptchaScript="true" @verify="validate"></vue-recaptcha>
+</template>
+<script>
+import VueRecaptcha from 'vue-recaptcha';
+export default {
+    components:{
+        VueRecaptcha
+    },
+    methods:{
+    validate(response){
+          this.$emit('validate', {response: response})
+    }
+    },
+    props:{
+        siteKey: String
+    }
+}
+</script>
--- src/services/dooService.js
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from "./RequestHandler";
-class DooService extends RequestHandler{
-   async getDooFormData(payload){
-       const response = await this.apiClient.post("/doo-mail", payload);
-       console.log(response);
-    }
-}
-const dooService = new DooService();
-export default dooService;
