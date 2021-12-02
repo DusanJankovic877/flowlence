0107d43edef52a6cc87292c5c664e7ea33dc2b2d
Dusan <dusanjankovic877@gmail.com>
2021-12-02 15:13:31 +0100

    I have styled textareas and form, i have added post title input and post section title input. I need to connect textareas that belong to post section title, same with images, and i have to make images and post section title to dinamicaly render

--- src/components/admin/CreatePostComponent.vue
+++ src/components/admin/CreatePostComponent.vue
@@ -2,20 +2,66 @@
     <div class="col-lg-7 m-auto create-post-form">
     <form @submit.prevent>
         <div class="mb-3 file-inputs">
+            <label for="blog-title" class="form-label">Naslov posta</label>
+            <input type="text" class="form-control" id="blog-title">
+        </div>
+        <div class="mb-3 file-inputs">
             <label for="formFileOne" class="form-label"><p>Prva slika</p></label>
-            <input name="img" @change="previewFiles" class="form-control" type="file" id="formFileOne" accept="image/*">
+            <input name="img" @change="previewFiles" class="form-control" type="file" id="formFileOne" accept="image/*">i slike treba vezati za naslove sekcija
         </div>
         <div class="mb-3 file-inputs">
             <label for="formFileTwo" class="form-label">Druga slika</label>
             <input name="img" @change="previewFiles" class="form-control" type="file" id="formFileTwo" accept="image/*">
         </div>
-        <div class="mb-3" v-for="(textarea, k) in blog.textareas" :key="k">
-            <label for="exampleFormControlTextarea1" class="form-label">Textarea{{k}}</label>
-            <textarea v-model="blog.textareas[k].text" class="form-control col-lg-3" id="exampleFormControlTextarea1" rows="3"></textarea>
-            <div v-if="textarea.id !== 0">
-            <button @click="handleDeleteTextarea(k)">Obrisi text areu</button>
+        <div class="mb-3 file-inputs">
+            <label for="blog-section-title" class="form-label">Naslov sekcije posta koji treba dinamicki generisati i  povezati sa text reonima koji mu pripadaju</label>
+            <input type="text" class="form-control" id="blog-section-title">
+        </div>
+        <div class="mb-3 text-areas" v-for="(textarea, k) in blog.textareas" :key="k">
+            <div class="row">
+                <label for="exampleFormControlTextarea1" class="form-label col-lg-8">Textarea{{textarea.id}}</label>
             </div>
-            <div v-else></div>
+            <div class="div-text col-lg-12 row">
+            <textarea v-model="blog.textareas[k].text" class="form-control text-area" id="exampleFormControlTextarea1" rows="3"></textarea>
+    
+                <div class="col-lg-2 buttons-up-down">
+                    <button class="btn btn-light ml-auto col-lg-5" @click="handleMoveUp(textarea)">
+                        <svg id="i-chevron-top" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 32 32" 
+                            width="14" 
+                            height="15" 
+                            fill="none" 
+                            stroke="currentcolor" 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="4">
+                            <path d="M30 20 L16 8 2 20" />
+                        </svg>
+                    </button>
+                    <button class="btn btn-dark col-lg-5" @click="handleMoveDown(textarea)">
+                        <svg id="i-chevron-bottom" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 32 32" 
+                            width="14" 
+                            height="15" 
+                            fill="none" 
+                            stroke="currentcolor" 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="4">
+                            <path d="M30 12 L16 24 2 12" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="delete-button  col-lg-1" v-if="textarea.id !== 0">
+                <button class="btn btn-danger" @click="handleDeleteTextarea(k)">Obrisi</button>
+                </div>
+                <div v-else></div>
+
+       
+
+            </div>
         </div>
    {{blog}}
             <button @click="handleAddTextarea(blog.textareas.length)">Dodaj novu text areu</button>
@@ -28,6 +74,7 @@ import { mapActions } from 'vuex';
 export default {
     data() {
         return{
+            //napraviti generisanje slika i generisanje naslova za odredjenu sekciju posta, i povezati text reone sa ti naslovom
             counter: 1,
             blog:{
                 images: [
@@ -47,7 +94,21 @@ export default {
                     }
                 ]
 
-            }
+            },
+            // move: function(textarea, delta){
+            //     var index = this.blog.textareas.indexOf(textarea);
+            //     var newIndex = index + delta;
+            //     if(newIndex < 0 || newIndex === this.blog.textareas.length)return;
+            //     var indexes = [index, newIndex].sort((a, b) => a - b);
+            //     this.blog.textareas.splice(indexes[0], 2, this.blog.textareas[indexes[1]], this.blog.textareas[indexes[0]])
+            // },
+            move: function(textarea, delta){
+            var index = this.blog.textareas.indexOf(textarea);
+            var newIndex = index + delta;
+            if(newIndex < 0 || newIndex == this.blog.textareas.length)return;
+            var indexes = [index, newIndex].sort((a, b) => a - b);
+            this.blog.textareas.splice(indexes[0], 2, this.blog.textareas[indexes[1]], this.blog.textareas[indexes[0]]);
+            },
         }
     },
     computed:{
@@ -77,6 +138,12 @@ export default {
             // this.deleteTextArea(k)
             this.blog.textareas.splice(k, 1);
         },
+        handleMoveUp(textarea){
+           this.move(textarea, -1);
+        },
+        handleMoveDown(textarea){
+           this.move(textarea, 1);
+        },
         handleCreatePost(){
             
         }
@@ -84,10 +151,48 @@ export default {
 }
 </script>
 <style >
-.file-inputs{
-    text-align: left;
+.create-post-form{
+    padding:15px;
+    background: rgba(209, 209, 209, 0.37) !important;
+
+}
+.file-inputs {
+    text-align: left !important;
+}
+.text-area{
+    width:70% ;
+
+}
+.text-areas{
+    width: 80%;
+}
+.text-areas:nth-child(odd) {
+
+    padding: 5px 0 10px 20px;
+  background: rgba(128, 128, 128, 0.815) !important;
+}
+.text-areas:nth-child(even) {
+    color: white;
+ background: rgba(48, 48, 48, 0.781) !important;
+    padding: 5px 0 10px 20px;
+
+}
+.div-text:nth-child(){
+    padding: 5px 0 10px 20px;
+    
+}
+.buttons-up-down{
+    margin: 0 !important;
+}
+.delete-button{
+   /* padding: 0; */
+   border-radius: 0;
+   border: none;
 }
 .file-inputs input{
+    width:70%;
+    /* float: left; */
+    margin:0 !important;
     border: none;
     border-radius: 0;
     border-bottom: 1px solid  black;
